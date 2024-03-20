ad18b2f547bdda0bf7f2ceee7f533438fc547ed1
Ygor Matos <ygorsilvamatos@hotmail.com>
2024-03-20 01:08:58 -0300

    corrigindo bug da pokedex nao aparecer

--- js/script.js
+++ js/script.js
@@ -4,25 +4,23 @@ const pokemonImage = document.querySelector('.pokemon_image');
 const pesquisa = document.querySelector('.pesquisa');
 const prev = document.querySelector('.btn-prev');
 const next = document.querySelector('.btn-next');
-
 const input = document.querySelector('.input_search')
 
 let searchPokemon=1;
 
 
-
 const fetchPokemon = async (pokemon) =>{
+
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
     if(APIResponse.status === 200){
         const data = await APIResponse.json();
         return data;
     }
-    
 
 }
 
-
 const renderPokemon = async (pokemon) =>{
+
     pokemonName.innerHTML = "Carregando...";
     const data = await fetchPokemon(pokemon);
 
@@ -43,29 +41,31 @@ const renderPokemon = async (pokemon) =>{
     
 }
 
-const pesquisando = () => {
-    event.preventDefault();
-
-    renderPokemon(input.value.toLowerCase());
-    
-
-}
 
 prev.addEventListener('click', ()=>{
+
     if(searchPokemon>1){
         searchPokemon-=1;
         renderPokemon(searchPokemon);
     }
+
 })
 
 next.addEventListener('click', ()=>{
 
     searchPokemon+=1;
     renderPokemon(searchPokemon)
+
 })
 
-pesquisa.addEventListener('submit', pesquisando);
+const pesquisando = () => {
 
+    event.preventDefault();
+    renderPokemon(input.value.toLowerCase());
+    
+}
+
+pesquisa.addEventListener('submit', pesquisando);
 
 
 renderPokemon(searchPokemon);
